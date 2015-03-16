71fdab3e50c63252f94c0fc7d2e9fbed5a9fc998
ThongNguyen <thongcs4@gmail.com>
2015-03-16 03:00:30 +0000

    thong comment

---
+++ Test/src/PrivateObject.java
@@ -0,0 +1,32 @@
+import java.lang.reflect.Field;
+
+/**
+ * 
+ */
+
+/**
+ * @author ThongNV2
+ *
+ */
+public class PrivateObject  {
+
+	private String privateString = null;
+
+	public PrivateObject(String privateString) {
+		this.privateString = privateString;
+	}
+	  
+	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException, SecurityException, NoSuchFieldException {
+		PrivateObject privateObject = new PrivateObject("The Private Value");
+
+		Field privateStringField = PrivateObject.class.
+		            getDeclaredField("privateString");
+
+		privateStringField.setAccessible(true);
+
+		String fieldValue = (String) privateStringField.get(privateObject);
+		System.out.println("fieldValue = " + fieldValue);
+
+	}
+
+}
